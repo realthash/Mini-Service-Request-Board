aca0f85b2f61f6436fae565f462e123e075e9c09
realthash <thashmikarathnayake11@gmail.com>
2026-05-16 14:03:41 +0530

    fix(JobReq) - fix the matching regular expression

---
+++ GlobalTNA-assessment/backend/src/models/JobReq.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const requestSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            trim: true
+        },
+        description: {
+            type: String,
+            required: [true, 'Description is required'],
+            trim: true
+        },
+        category: {
+            type: String,
+            enum: {
+                values: ['Plumbing', 'Electrical', 'Painting'],
+                message: '{VALUE} is not a valid category'
+            }
+        },
+        location: {
+            type: String,
+            trim: true
+        },
+        contactNumber: {
+            type: String,
+            trim: true
+        },
+        contactEmail: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            match: [/^\S+@\S+\.\S+$/, 'Please provide a valid mail']
+        },
+        status: {
+            type: String,
+            enum: {
+                values: ['Open', 'In progress', 'Closed'],
+                message: '{VALUE} is not a valid status'
+            },
+            default: 'Open'
+        }
+    },
+    {
+        timeStamps: { createdAt: true, updatedAt: false }
+    }
+)
+
+module.exports = mongoose.model('JobReq', requestSchema)
